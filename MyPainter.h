0585a199e8f174dc8b50ce4868ee9168d92078ed
Unknown <zzespoozz@gmail.com>
2018-01-31 21:08:41 +0300

    аливка треугольника     // с использованием z-буфера для отсечения ненужных пикселей

--- MyPainter.h
+++ MyPainter.h
@@ -7,6 +7,7 @@
 
 #include <cmath>
 #include <utility>
+#include <limits>
 
 #include "model.h"
 
@@ -91,9 +92,13 @@ public:
             // 2.2 Заливка треугольников модели рандомным цветом
 //            random_fill_model(_model, image);
 
-            // 2.3 Заливка треугольников модели с учетом нормалей и направления света
             Vec3f light_dir(0,0,-1);    // вектор направления света
-            fill_model_with_normal(_model, image, light_dir);
+            // 2.3 Заливка треугольников модели с учетом нормалей и направления света
+//            fill_model_with_normal(_model, image, light_dir);
+
+            // 3.1 Заливка треугольников модели с учетом нормалей и направления света
+            // и с использованием z-буфера для отсечения ненужных пикселей
+            fill_model_with_z_buffer(_model, image, light_dir);
         }
         else
         {
@@ -198,6 +203,39 @@ public:
         }
     }
 
+    // Заливка треугольника
+    // с использованием z-буфера для отсечения ненужных пикселей
+    void fill_triangle(Vec3i t0, Vec3i t1, Vec3i t2, Image &image, Pixel color, int *zbuffer)
+    {
+        int width = image.width();
+        int height = image.height();
+
+        if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+        if (t0.y>t1.y) std::swap(t0, t1);
+        if (t0.y>t2.y) std::swap(t0, t2);
+        if (t1.y>t2.y) std::swap(t1, t2);
+        int total_height = t2.y-t0.y;
+        for (int i=0; i<total_height; i++) {
+            bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+            int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+            float alpha = (float)i/total_height;
+            float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+            Vec3i A =               t0 + Vec3f(t2-t0)*alpha;
+            Vec3i B = second_half ? t1 + Vec3f(t2-t1)*beta : t0 + Vec3f(t1-t0)*beta;
+            if (A.x>B.x) std::swap(A, B);
+            for (int j=A.x; j<=B.x; j++) {
+                float phi = B.x==A.x ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
+                Vec3i P = Vec3f(A) + Vec3f(B-A)*phi;
+                int idx = P.x+P.y*width;
+                if (zbuffer[idx]<P.z) {
+                    zbuffer[idx] = P.z;
+                    image.set(P.x, P.y, color);
+                }
+            }
+        }
+    }
+
+
     // Отрисовка контуров треугольников модели
     void draw_model(Model *model, Image &image, Pixel color)
     {
@@ -275,6 +313,41 @@ public:
             if (intensity>0) {
                 Pixel color = {intensity*255, intensity*255, intensity*255, 255};
                 fill_triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, color);
+            }
+        }
+    }
+
+
+    // Заливка треугольников модели с учетом нормалей и направления света
+    // и с использованием z-буфера для отсечения ненужных пикселей
+    // light_dir - вектор направление света
+    void fill_model_with_z_buffer(Model *model, Image &image, Vec3f &light_dir)
+    {
+        int width = image.width();
+        int height = image.height();
+        const int depth  = 255;
+
+        // Поскольку у нас экран двумерный, то z-буфер тоже должен быть двумерным:
+        int *zbuffer = new int[width*height];
+        for (int i=0; i<width*height; i++) {
+            zbuffer[i] = std::numeric_limits<int>::min();
+        }
+
+        for (int i=0; i<model->nfaces(); i++) {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            for (int j=0; j<3; j++) {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] = Vec3i((v.x+1.)*width/2., (v.y+1.)*height/2., (v.z+1.)*depth/2.);
+                world_coords[j]  = v;
+            }
+            Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+            n.normalize();
+            float intensity = n*light_dir;
+            if (intensity>0) {
+                Pixel color = {intensity*255, intensity*255, intensity*255, 255};
+                fill_triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, color, zbuffer);
             }
         }
     }
--- QtGui.pro
+++ QtGui.pro
@@ -15,6 +15,7 @@ TEMPLATE = app
 SOURCES += main.cpp\
         mainwindow.cpp\
         MyPainter.cpp\
+        geometry.cpp\
         model.cpp
 
 HEADERS  += mainwindow.h\
